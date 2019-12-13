2e2d597fee255cc12df3af3060fca9764d2563a5
saja amer <sajaamer86@gmail.com>
2019-12-13 14:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Add your code here
+<<<<<<< HEAD
 function submitData(name, email){
     return fetch("http://localhost:3000/users",
         {
@@ -26,3 +27,26 @@ function submitData(name, email){
 
 
 
+=======
+function submitData(name,email){
+  const URL= 'http://localhost:3000/users';
+  const requestOptions={
+    header:{
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+    },
+    method:'POST',
+    body: JSON.stringify({
+      name:name,
+      email:email
+    })
+  }
+  return fetch(URL,requestOptions);
+}
+submitData("john","john@invisible.com")
+.then(res=>{
+  return res.json();
+})
+//.then(json=>)
+//.catch(err=>document.body.innerHTML=err.message)
+>>>>>>> c511c20711e834a23866b74c487df575c40adfde
